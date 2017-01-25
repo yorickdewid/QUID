7113a606fec43ff52bcef98db4792d7ffc37b815
Yorick de Wid <yorickdewid@users.noreply.github.com>
2017-01-25 15:26:58 +0100

    Update year

--- include/quid.h
+++ include/quid.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014, Yorick de Wid <tech at quenza dot net>
+ * Copyright (c) 2012-2017, Yorick de Wid <tech at quenza dot net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
